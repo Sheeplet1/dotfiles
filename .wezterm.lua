48d184d82cc71dcb1f0a68c575e1370ac7bfddc4
Anthony <42196548+Sheeplet1@users.noreply.github.com>
2024-05-08 09:42:12 +1000

    feat: .wezterm.lua hide tab bar

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -20,11 +20,13 @@ config.font = wezterm.font("JetBrains Mono")
 config.max_fps = 120
 
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
+
+config.hide_tab_bar_if_only_one_tab = true
 
 -- and finally, return the configuration to wezterm
 return config
