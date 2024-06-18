3fe307a8e5212e39a72bdaac9b9a58df0e5f9d8d
Sheeplet <42196548+Sheeplet1@users.noreply.github.com>
2024-06-18 17:38:21 +1000

    transparent for all modes lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,11 +4,17 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     event = "VeryLazy",
     opts = function()
-      local auto_theme_custom = require "lualine.themes.auto"
-      auto_theme_custom.normal.c.bg = "none"
+      local theme = require "lualine.themes.auto"
+
+      for _, mode in pairs(theme) do
+        if mode.c and mode.c.bg then
+          mode.c.bg = "none"
+        end
+      end
+
       return {
         options = {
-          theme = auto_theme_custom,
+          theme = theme,
           component_separators = "|",
           section_separators = { left = "", right = "" },
         },
