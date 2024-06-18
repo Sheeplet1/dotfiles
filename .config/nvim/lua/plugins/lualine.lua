9c184009e926e600d6c53e47db1aa1770f0c5231
Sheeplet <42196548+Sheeplet1@users.noreply.github.com>
2024-06-18 16:45:07 +1000

    make lualine transparent

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,13 +3,16 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     event = "VeryLazy",
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = "auto",
-        component_separators = "|",
-        section_separators = { left = "", right = "" },
-      },
-    },
+    opts = function()
+      local auto_theme_custom = require "lualine.themes.auto"
+      auto_theme_custom.normal.c.bg = "none"
+      return {
+        options = {
+          theme = auto_theme_custom,
+          component_separators = "|",
+          section_separators = { left = "", right = "" },
+        },
+      }
+    end,
   },
 }
