7fee0b498e290ca1e575e6ce5868676ae0700591
Sheeplet <42196548+Sheeplet1@users.noreply.github.com>
2024-08-05 17:51:42 +1000

    cleaning up

--- .config/nvim-mine/lua/plugins/gitsigns.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		"lewis6991/gitsigns.nvim",
-		event = "User FilePost",
-		config = function(_, opts)
-			return require("plugins.configs.others").gitsigns.setup(opts)
-		end,
-	},
-}
--- .config/nvim-mine/lua/plugins/oil.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    'stevearc/oil.nvim',
-    event = 'VeryLazy',
-    cmd = 'Oil',
-    opts = {},
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-  },
-}
