4fd2a4ceaf5e707e601d4a66e104aa17151015b1
Sheeplet <42196548+Sheeplet1@users.noreply.github.com>
2024-08-27 19:27:04 +1000

    update

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,10 +1,10 @@
 return {
-  {
-    "echasnovski/mini.surround",
-    version = false,
-    event = "BufEnter",
-    config = function()
-      require("mini.surround").setup {}
-    end,
-  },
+  --   {
+  --     "echasnovski/mini.surround",
+  --     version = false,
+  --     event = "BufEnter",
+  --     config = function()
+  --       require("mini.surround").setup {}
+  --     end,
+  --   },
 }
