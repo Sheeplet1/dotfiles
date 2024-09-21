715ef768f3cec1991582cfa20a754dc96d8aa188
Anthony <42196548+Sheeplet1@users.noreply.github.com>
2024-09-21 20:59:13 +1000

    remove tokyonight

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,24 +1,24 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = "VeryLazy",
-    opts = function()
-      -- local theme = require "lualine.themes.auto"
-      local theme = require "configs.tokyonight-lualine"
-      for _, mode in pairs(theme) do
-        if mode.c and mode.c.bg then
-          mode.c.bg = "none"
-        end
-      end
-
-      return {
-        options = {
-          theme = theme,
-          component_separators = "|",
-          section_separators = { left = "", right = "" },
-        },
-      }
-    end,
-  },
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   dependencies = { "nvim-tree/nvim-web-devicons" },
+  --   event = "VeryLazy",
+  --   opts = function()
+  --     -- local theme = require "lualine.themes.auto"
+  --     local theme = require "configs.tokyonight-lualine"
+  --     for _, mode in pairs(theme) do
+  --       if mode.c and mode.c.bg then
+  --         mode.c.bg = "none"
+  --       end
+  --     end
+  --
+  --     return {
+  --       options = {
+  --         theme = theme,
+  --         component_separators = "|",
+  --         section_separators = { left = "", right = "" },
+  --       },
+  --     }
+  --   end,
+  -- },
 }
