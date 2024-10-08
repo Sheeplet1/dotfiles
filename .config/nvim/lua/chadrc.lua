697ad699663294e3a436b3a02df69f7df5269af5
Sheeplet <42196548+Sheeplet1@users.noreply.github.com>
2024-10-09 09:28:50 +1100

    fix: lspconfig wrong options being provided

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -4,7 +4,7 @@
 ---@type ChadrcConfig
 local M = {
   base46 = {
-    theme = "rosepine",
+    theme = "chadracula-evondev",
     integrations = {
       "cmp",
       "todo",
--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,16 +1,16 @@
 local nvlsp = require "nvchad.configs.lspconfig"
 
-local nvchad_on_attach = nvlsp.on_attach
-local on_attach = function(client, bufnr)
-  nvchad_on_attach(client, bufnr)
-  -- extend on_attach here
-end
+-- local nvchad_on_attach = nvlsp.on_attach
+-- local on_attach = function(client, bufnr)
+--   nvchad_on_attach(client, bufnr)
+--   -- extend on_attach here
+-- end
 
-local lspconfig = require "lspconfig"
+local on_attach = nvlsp.on_attach
 local on_init = nvlsp.on_init
 local capabilities = nvlsp.capabilities
 
--- leaving this here incase the inline function does not work
+-- leaving this here incase I want to use this function again
 -- local function organise_imports()
 --   local params = {
 --     command = "_typescript.organizeImports",
@@ -36,7 +36,7 @@ local servers = {
       "typescriptreact",
       "typescript.tsx",
     },
-    root_dir = lspconfig.util.root_pattern(
+    root_dir = require("lspconfig").util.root_pattern(
       ".eslintrc.js",
       ".eslintrc.json",
       ".eslintrc.yml",
@@ -44,31 +44,18 @@ local servers = {
       "package.json"
     ),
   },
-
   ts_ls = {
     settings = {
       separate_diagnostic_server = true,
       public_diagnostic_on = "insert_leave",
       tsserver_plugins = {},
     },
-    commands = {
-      OrganiseImports = {
-        (function()
-          local params = {
-            command = "_typescript.organizeImports",
-            arguments = { vim.api.nvim_buf_get_name(0) },
-          }
-          vim.lsp.buf.execute_command(params)
-        end)(),
-        description = "Organise Imports",
-      },
-    },
   },
 
   gopls = {
     cmd = { "gopls" },
     filetypes = { "go", "gomod", "gowork", "gotmpl" },
-    root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+    root_dir = require("lspconfig").util.root_pattern("go.work", "go.mod", ".git"),
     settings = {
       gopls = {
         completeUnimported = true,
@@ -93,5 +80,5 @@ for name, opts in pairs(servers) do
   opts.on_attach = on_attach
   opts.capabilities = capabilities
 
-  lspconfig[name].setup(opts)
+  require("lspconfig")[name].setup(opts)
 end
